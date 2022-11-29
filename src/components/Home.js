e150a7a8a535ab0e8f4c5f377f6861f1d48ec067
angie10 <delacruzvargasangelica26@gmail.com>
2022-11-29 08:52:06 -0500

    pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,30 @@
 import NewsApi from "./newsApi"
+import { Pagination } from "react-bootstrap";
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
+	let active = 2;
+	let items = [];
+	for (let number = 1; number <= 5; number++) {
+		items.push(
+			<Pagination.Item key={number} active={number === active}>
+				{number}
+			</Pagination.Item>,
+		);
+	}
+	const paginationBasic = (
+		<div>
+		  <Pagination>{items}</Pagination>
+		  </div>
+	)
 	return (
 		<>
 			<h2>Home Page</h2>
 			<NewsApi />
+			{paginationBasic}
+
 		</>
 	)
 }
