72e22191e8a691f7aff17afa15bc91606f6d93a5
angie10 <delacruzvargasangelica26@gmail.com>
2022-12-01 14:19:35 -0500

    added buttons to each article

--- src/components/showArticles.js
+++ src/components/showArticles.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import './components.css'
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 
-const ShowArticles = ({title, description, url, urlToImage, content}) => {
+
+const ShowArticles = ({ title, description, url, urlToImage, content }) => {
     return (
-        <div style={{display: 'inline-block'}}>
-            
-            <Card style={{width: '300px', height: '200px', overflow: 'auto'}}>
-            <img src={urlToImage} alt={urlToImage}/>
-            <h3><a href={url}>{title}</a></h3>
-            <p>{description}</p>
-            </Card>
-           
-        </div>
+        <>
+            <div style={{ display: 'inline-block' }}>
+                <div>
+                    <Card style={{ width: '400px', height: '600px', margin: '15px', overflow: 'auto', border: 'none' }}>
+                        <img src={urlToImage} />
+                        <h5 style={{ color: 'white' }}><a href={url}>{title}</a></h5>
+                        <p>{description}</p>
+                        <div style={{ display: 'inline-block', textAlign: 'center' }}>
+                            <Button style={{ marginRight: '10px', width: '150px' }}>
+                                <a href={url} style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>Read More</a>
+                            </Button>
+                            <Button style={{ width: '50px' }}>
+                                <FontAwesomeIcon icon={faBookmark} />
+                            </Button>
+                        </div>
+                    </Card>
+
+                </div>
+            </div>
+        </>
     )
 }
 
