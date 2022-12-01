feb219c771c17da282fd5995a4ee1f20ae41958e
angie10 <delacruzvargasangelica26@gmail.com>
2022-12-01 14:20:46 -0500

    added hompage image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,22 @@
-import NewsApi from "./newsApi"
+import NewsApi from "./NewsApi"
 import { Pagination } from "react-bootstrap";
+import AdvancedExample from "./Pagination";
+import IMAGE from '../images/tech-img.jpg'
+
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
-	let active = 2;
-	let items = [];
-	for (let number = 1; number <= 5; number++) {
-		items.push(
-			<Pagination.Item key={number} active={number === active}>
-				{number}
-			</Pagination.Item>,
-		);
-	}
-	const paginationBasic = (
-		<div>
-		  <Pagination>{items}</Pagination>
-		  </div>
-	)
 	return (
 		<>
-			<h2>Home Page</h2>
+			<div>
+				<img src={IMAGE} alt='' style={{width: '100%', maxWidth: '100%', height: '500px', margin: '15px'}}/>
+				<div style={{position: 'absolute', bottom: '10px', left: '18px', color: 'blue'}}>A place where you can find the latest news in tech.</div>
+			</div>
 			<NewsApi />
-			{paginationBasic}
+			<AdvancedExample/>
+
 
 		</>
 	)
